0c586463bb3ff721dbf56d35c1b882a0a9bcb412
Aidar-Abdurazakov <aidarabdurazakov05@outlook.com>
2026-08-11 20:21:19 +0600

    my last commit

--- handlers/routes.py
+++ handlers/routes.py
@@ -1,4 +1,3 @@
-
 from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
@@ -6,31 +5,37 @@ from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
 router = Router()
 
+
 def get_main_reply_keyboard():
     keyboard = ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="О боте")],
-            [KeyboardButton(text="Старт"), KeyboardButton(text="Помощь")]
+            [KeyboardButton(text="Старт"), KeyboardButton(text="/menu")]
         ],
         resize_keyboard=True
     )
+
     return keyboard
 
+
 @router.message(Command("start"))
-@router.message(F.text.lower == "Старт")
+@router.message(F.text.lower() == "старт")
 async def start(message: Message):
+
     await message.answer(
-        "Привет! Я твой личный ассистент.\n\n"
-        "Напиши /help для помощи"
+        "Добро пожаловать в наше кафе!\n\n"
+        "Напиши /menu для помощи",
+        reply_markup=get_main_reply_keyboard()
     )
 
 
-@router.message(Command("help"))
+@router.message(Command("menu"))
 async def help(message: Message):
+
     await message.answer(
         "Команды:\n"
         "/start — запуск\n"
-        "/help — список команд\n"
+        "/menu — список команд\n"
         "/about — про нас",
         reply_markup=get_main_reply_keyboard()
     )
@@ -38,11 +43,14 @@ async def help(message: Message):
 
 @router.message(Command("about"))
 async def about(message: Message):
+
     await message.answer(
         f"Дорогой {message.from_user.full_name}, "
-        "меня зовут Айдар, и я твой личный ассистент."
+        "добро пожаловать в наше кафе 'Geeks'."
     )
 
-@router.message(Command("start"))
-async def start(message: Message):
-    await message.answer("Напиши что нибудь")
+
+@router.message(F.text.lower() == "пока")
+async def goodbye(message: Message):
+
+    await message.answer("До встречи!")
--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from os import getenv
 import asyncio
+
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
+
 from handlers.routes import router
+
+
 load_dotenv()
 
 TOKEN = getenv("BOT_TOKEN")
 
 dp = Dispatcher()
+
 dp.include_router(router)
+
 
 async def main():
     bot = Bot(token=TOKEN)
+
     print("Start...")
 
     await dp.start_polling(bot)
